ffb7e60604a0f79e892fdc4397fdb65984030050
constoner <constantin.kalinin@gmail.com>
2023-03-23 23:06:27 +0300

    убрал изменение цвета хэдера при скроле

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 import './utils/ios-checker';
 import {iosVhFix} from './utils/ios-vh-fix';
-import {StickyHeader} from './modules/sticky-header/sticky-header';
 import './utils/focus-visible-polyfill';
 import './utils/focus-lock';
 import './utils/scroll-lock';
@@ -29,9 +28,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    const stickyHeader = new StickyHeader();
-    stickyHeader.init();
-
     initModals();
     const form = new Form();
     window.form = form;
